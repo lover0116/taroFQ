ad4d5687ef5955d95d4f3475151b4e2c16c077e6
xuzhi31672 <780272033@qq.com>
2024-12-02 17:40:18 +0800

    增加按钮显示隐藏答案

--- src/pages/qAndA/index.tsx
+++ src/pages/qAndA/index.tsx
@@ -1,17 +1,20 @@
 import {View} from "@tarojs/components";
 import data from './data';
-import React from "react";
+import React, {useState} from "react";
+import {AtButton} from "taro-ui";
 export default function QA() {
-
+  const [isAsk, setIsAsk] = useState(true);
   return (
     <View style={{fontSize: '0.8rem', padding: '10px'}}>
-
+      <AtButton type="primary" onClick={()=> setIsAsk(!isAsk)}>{isAsk?'隐藏答案':'显示答案'}</AtButton>
       {
         data.map((i:any, idx:number)=>{
           return (
             <View key={idx}>
               <View style={{margin: '20px 0', color: 'red' }} dangerouslySetInnerHTML={{__html: `${idx+1}、${i.fq}`}} />
-              <View dangerouslySetInnerHTML={{__html: `答：${i.ask}`}} />
+              {
+                <View style={{opacity: isAsk?1:0}} dangerouslySetInnerHTML={{__html: `答：${i.ask}`}} />
+              }
             </View>
           )
         })
